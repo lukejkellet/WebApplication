ec55d3dc1cd07bf453e7edfb3ae4b85f7cee73fb
Luke Kellet <lukejameskellet@hotmail.com>
2023-10-03 13:44:17 +0100

    Add project files.

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication.Controllers
+{
+    // ../api/Home
+    public class HomeController : ApiController
+    {
+        private string[] S = { "apple", "Banana", "Orange" };
+        public string[] Get()
+        {
+            return S;
+        }
+        // ...api/Home/{id}
+        public string Get(int id)
+        {
+            return S[id];
+        }
+    }
+}
